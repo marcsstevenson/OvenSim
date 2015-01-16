8fbd9b1950ed6a0cddec5dd80efc50f4ffc18142
essentialresources <essentialresources@outlook.com>
2015-01-17 12:35:06 +1300

    w/e

---
+++ Tests/spec/Timer.CountUp.Spec.js
@@ -0,0 +1,78 @@
+
+/// <reference path="//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min.js" />
+/// <reference path="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" />
+/// <reference path="//cdnjs.cloudflare.com/ajax/libs/jasmine/2.0.0/jasmine.js"/>
+/// <reference path="../../Lib/moment-2.8.4.min.js" />
+/// <reference path="../lib/jasmine-2.1.3/jasmine.js" />
+
+
+describe("Oven", function () {
+    var ovenViewModel;
+
+    beforeEach(function () {
+        ovenViewModel = new OvenViewModel();
+    });
+
+    describe("when power is On", function () {
+        describe("when time is running", function () {
+            it("Oven shall be heating", function () {
+
+                ovenViewModel.TimerStartValue(10);
+
+                ovenViewModel.StartTimer();
+
+                expect(ovenViewModel.IsHeating()).toEqual(true);
+            });
+
+            it("TimerRunning shall be true", function () {
+
+                ovenViewModel.TimerStartValue(10);
+                ovenViewModel.StartTimer();
+
+                expect(ovenViewModel.TimerRunning()).toEqual(true);
+            });
+
+            it("TimerStarted shall be true", function () {
+
+                ovenViewModel.TimerStartValue(10);
+                ovenViewModel.StartTimer();
+
+                expect(ovenViewModel.TimerStarted()).toEqual(true);
+            });
+
+            it("Timer shall not exceed the max time when timing up", function () {
+                ovenViewModel.TimerStartValue(-1);
+                ovenViewModel.StartTimer();
+                ovenViewModel.ClearTimerCountdownTimer(); //We don't want a live timer
+
+                var duration = moment.duration('16:38:58'); ///999 minutes less a second
+
+                ovenViewModel.TimerCurrentValue(duration); //Shortcut
+
+                //Now tick 62 times and make sure that we're not over 999 minutes
+                for (var i = 0; i <= 62; i++) {
+                    ovenViewModel.SetTimerTimerNextValue();
+                }
+
+                var nowInMinutes = ovenViewModel.TimerCurrentValue().asMinutes();
+
+                expect(nowInMinutes).toEqual(999);
+            });
+
+            it("Timer shall not go below 0 seconds when timing down", function () {
+                ovenViewModel.TimerStartValue(1);
+                ovenViewModel.StartTimer();
+                ovenViewModel.ClearTimerCountdownTimer(); //We don't want a live timer
+
+                //Now tick 62 times and make sure that we're not over 999 minutes
+                for (var i = 0; i <= 62; i++) {
+                    ovenViewModel.SetTimerTimerNextValue();
+                }
+
+                var nowInSeconds = ovenViewModel.TimerCurrentValue().asSeconds();
+
+                expect(nowInSeconds).toEqual(0);
+            });
+        });
+    });
+});
